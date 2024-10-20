815bda4011acc8a680e59ba34478f303c806681e
Kevin Zamora <kevin.zamora.amela@gmail.com>
2024-10-20 09:34:39 +0200

    init repo / adding Spring Boot project's base structure

---
+++ src/test/java/com/kevinzamora/Ej_Modulo_2_Sping_Boot_Curso_Testing_con_Java_KZ/EjModulo2SpingBootCursoTestingConJavaKzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinzamora.Ej_Modulo_2_Sping_Boot_Curso_Testing_con_Java_KZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjModulo2SpingBootCursoTestingConJavaKzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
